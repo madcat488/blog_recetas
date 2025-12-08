8530852c639528b991b4603b1140a6518344b007
madcat488 <catriel.a.acosta@gmail.com>
2025-12-08 17:44:02 -0300

    initial_commit

---
+++ blog/apps/usuarios/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class Usuario(AbstractUser):
+    nombre = models.CharField(max_length=30, blank=True)
+    apellido = models.CharField(max_length=30, blank=True)
+    fecha_nacimiento = models.DateField("Fecha Nacimiento",null=True, blank=True)
+    email = models.EmailField(unique=True)
+    colaborador = models.BooleanField("Colaborador", default=False)
+    imagen = models.ImageField(upload_to='usuarios', null=True, blank=True, default='usuarios/default_user.png')
+    
+    class Meta:
+        ordering = ('-nombre',)
+        
+    def __str__(self):
+        return self.nombre
+    
